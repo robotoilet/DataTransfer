03247e3ea748401c87049e2438282e644ab1a2c5
Alecs Geuder <al.geuder@gmail.com>
2014-11-25 17:14:49 +0000

    rewritten to use TestSensor(unfinished)

--- log/log.ino
+++ log/log.ino
@@ -1,98 +1,93 @@
 #include <FileIO.h>
 #include "Timer.h"
+#include "TestSensor.h"
 
-int SECOND = 1000;
-int MINUTE = 60 * SECOND;
-int HOUR = 60 * MINUTE;
+// for MQTT stuff
+#include <SPI.h>
+#include <YunClient.h>
+#include <PubSubClient.h>
+
+#define SECOND 1000L
+#define MINUTE 60 * SECOND
+#define HOUR 60 * MINUTE
+
+byte dataPointCounter = 0;
+
 char* LOGFILE = "/mnt/sda1/datalog.txt";
+File dataFile;
 
 Timer t;
-String xData;
-String yData;
-String zData;
-String xDataGrouped;
-String yDataGrouped;
-String zDataGrouped;
+
+// for our own 'checksum' calculation
+unsigned long timeChecksum;
+unsigned int valChecksum;
+
+
+// WIFI-specifics: Update these with values suitable for your network.
+byte server[] = { 10, 10, 63, 129 };
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  // handle message arrived
+}
+
+YunClient yunClient;
+PubSubClient client(server, 1883, callback, yunClient);
+
+TestSensor testSensorOne(1); // create TestSensor with it's UID
+TestSensor testSensorTwo(2); // create TestSensor with it's UID
 
 void setup() {
   Bridge.begin();
   Serial.begin(9600);
   FileSystem.begin();
 
-  Serial.println("Filesystem datalogger\n");
-
-  // initialise Strings for each sensor, that will build up timestamp-value
-  // pairs (as in add them to the strings) until they are written to a file
-  resetCollectedData(); 
+  while(!Serial);  
+  Serial.println("data transfer of fake data\n");
   
+  dataFile = FileSystem.open(LOGFILE + getTimestamp(, FILE_APPEND);
+    
   // task scheduling..
   // (1) sensor data collection
-  t.every(5 * SECOND, collectDataForSensorX);
-  t.every(10 * SECOND, collectDataForSensorY);
-  t.every(20 *SECOND, collectDataForSensorZ);
-  // (2) data logging
-  t.every(20 * SECOND, logData);
+  t.every(5 * SECOND, writeDataForSensorOne);
+  Serial.println("just saved some data for sensorOne..");
+  t.every(10 * SECOND, writeDataSensorTwo);
+  Serial.println("just saved some data for sensorTwo..");
+  
   // (3) data transfer to server
   t.every(MINUTE, sendData);
+
+}
+
+bool checkCounter() {
+  if (dataPointCounter < 50) {
+    dataPointCounter ++;
+    return true;
+  } else {
+    dataFile.close();
+    // 1. send the dataFile
+    // 2. rename this particular dataFile to *.sent
+    // 3. create again a new file with timestamped name
+    dataFile = FileSystem.open(LOGFILE, FILE_APPEND);
+    return false;
+  }
+}
+
+writeDataForSensorOne() {
+  // call checkPointer function
+  if checkCounter() {
+    testSensorOne.collectData(dataFile);
+  }
+}
+
+writeDataForSensorTwo() {
+  // call checkPointer function
+  if checkCounter() {
+    testSensorOne.collectData(dataFile);
+  }
 }
 
 void loop () {
   t.update();
-}
-
-// 1. Data Collection (creating fake Sensor values):
-//    helper functions that build *strings* of data, one for each sensor
-String createSensorData() {
-  return " (" + getTimeStamp() + " " + random(300) + ")";
-}
-String getTimeStamp() {
-  String result;
-  Process time;
-  time.begin("date");
-  time.addParameter("+%s"); // as seconds since 1970
-  time.run();
-
-  while(time.available()>0) {
-    char c = time.read();
-    if(c != '\n')
-      result += c;
-  }
-
-  return result;
-}
-void collectDataForSensorX() {
-  xData += createSensorData();
-}
-void collectDataForSensorY() {
-  yData += createSensorData();
-}
-void collectDataForSensorZ() {
-  zData += createSensorData();
-}
-
-// 2. Data Logging -- writes the Strings containing sensor data to a file
-//    and resets them to be empty.
-void logData() {
-  Serial.println(xData);
-  Serial.println(yData);
-  Serial.println(zData);
-  File dataFile = FileSystem.open(LOGFILE, FILE_APPEND);
-  if (dataFile) {
-    dataFile.println(xData);
-    dataFile.println(yData);
-    dataFile.println(zData);
-    dataFile.close();
-    resetCollectedData();
-  }
-  else {
-    Serial.println("error opening datalog.txt");
-  }
-
-}
-void resetCollectedData() {
-  xData = "SensorX";
-  yData = "SensorY";
-  zData = "SensorZ";
 }
 
 // 3. Data Transfer to server
@@ -102,16 +97,34 @@ void sendData() {
   yDataGrouped = "SensorY";
   zDataGrouped = "SensorZ";
   readData();
-  Serial.println(xDataGrouped);
-  Serial.println(yDataGrouped);
-  Serial.println(zDataGrouped);
+  dataToSend = xDataGrouped + yDataGrouped + zDataGrouped;
+  Serial.println("Data to send: " + dataToSend);
+  dataToSend.toCharArray(sendBuffer, 300);
+  // generate a hex encoded
+  
+  String stringsum = String(timeChecksum) + "_" + String(valChecksum);
+  char checksum[stringsum.length()];
+  stringsum.toCharArray(checksum, sizeof(checksum));
+  
+  // now send it: try to connect, if there's a connection, publish
+  // the stuff
+  if (client.connect("siteX", "punterX", "punterX")) {
+    client.publish(checksum, sendBuffer);
+  }
+  
+  // tidy up the checksums
+  timeChecksum = 0;
+  valChecksum = 0;
 }
+
 void readData() {
   Serial.println("reading data");
   File f = FileSystem.open(LOGFILE, FILE_APPEND);
-  String line = readLine(f);
   String sensorName;
   if (f) {
+    Serial.println("readData: file object there");
+    String line = readLine(f);
+    Serial.println("readData: have a line: " + line);
     while (line != "EOF") {
       sensorName = getSensorName(line);
       if (sensorName == "sensorX") 
@@ -125,12 +138,14 @@ void readData() {
     f.close();
   }
 }
+
 String readLine(File f) {
   int b = f.read();
   while (b != -1) {
     char c = (char)b;
     String line = "";
-    while (c != '\n') {
+    if (c == '\n') {
+      return line;
       line += c;
       char c = (char)f.read();
     }
@@ -138,13 +153,14 @@ String readLine(File f) {
   }
   return "EOF";
 }
+
 String getSensorName(String line) {
   int firstComma = line.indexOf(',');
   return line.substring(0, firstComma);
 }
 String getSensorData(String line) {
   int firstComma = line.indexOf(',');
-  return line.substring(firstComma+1, line.length());
+  return line.substring(firstComma + 1, line.length());
 }
 
 
